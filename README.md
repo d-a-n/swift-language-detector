a012c24307054aa3f42fa5cec78e2020f36e92f7
dan <dan@imake.io>
2017-08-12 16:29:53 +0200

    add readme and more tests

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# swift-language-detector
+# LanguageDetector
+
+LanguageDetector tries to detect the language of a given text by comparing it against the top 1000 words of every language.
+
+```
+import LanguageDetector
+
+let text1 = "The quick brown fox jumps over the lazy dog"
+let text2 = "Zwölf Boxkämpfer jagen Eva quer über den großen Sylter Deich"
+
+let text3 = "abc 123"
+
+LanguageDetector.detect(text1) // -> "en"
+LanguageDetector.detect(text2) // -> "de"
+LanguageDetector.detect(text3) // -> nil
+
+```
+
+## License
+MIT
--- Tests/LanguageDetectorTests/LanguageDetectorTests.swift
+++ Tests/LanguageDetectorTests/LanguageDetectorTests.swift
@@ -6,16 +6,19 @@ class LanguageDetectorTests: XCTestCase {
         XCTAssertEqual(LanguageDetector.detect(text: "The quick brown fox jumps over the lazy dog"), Optional("en"))
     }
     func testDe() {
-        XCTAssertEqual(LanguageDetector.detect(text: "Der schnelle braune Fuchs springt über den faulen Hund"), Optional("de"))
+        XCTAssertEqual(LanguageDetector.detect(text: "Zwölf Boxkämpfer jagen Eva quer über den großen Sylter Deich"), Optional("de"))
     }
     func testFr() {
-        XCTAssertEqual(LanguageDetector.detect(text: "Le renard brun rapide saute sur le chien paresseux"), Optional("fr"))
+        XCTAssertEqual(LanguageDetector.detect(text: "Buvez de ce whisky que le patron juge fameux"), Optional("fr"))
     }
     func testEs() {
         XCTAssertEqual(LanguageDetector.detect(text: "El zorro marrón rápido salta sobre el perro perezoso"), Optional("es"))
     }
     func testIt() {
         XCTAssertEqual(LanguageDetector.detect(text: "La volpe marrone veloce salta sul cane pigro"), Optional("it"))
+    }
+    func testZh() {
+        XCTAssertEqual(LanguageDetector.detect(text: "千 字 文"), Optional("zh_cn"))
     }
     func testUnknown() {
         XCTAssertEqual(LanguageDetector.detect(text: "abc def"), nil)
